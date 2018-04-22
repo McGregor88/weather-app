771c332c6de9baf2dc3370b8e1e1a021b35b68f6
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-04-22 20:18:13 +0300

    add weather API

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>Weather Component</div>
-        );
-    }
-};
+const Weather = props => (
+    <div className="weather-block">
+        <ul className="weather-list">
+            { props.city && props.country && <li className="weather-list-item">Location: { props.city }, { props.country }</li> }
+            { props.temperature && <li className="weather-list-item">Temperature: { props.temperature }</li> }
+            { props.humidity && <li className="weather-list-item">Humidity: { props.humidity }</li> }
+            { props.description && <li className="weather-list-item">Conditions: { props.description }</li> }
+            { props.error && <li className="weather-list-item text-error">{ props.error }</li> }
+        </ul>
+    </div>
+);
 
 export default Weather;
