a22fc68ecf77882fe9e17573ed94d4848f195bda
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-04-22 22:42:59 +0300

    add styles

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,32 @@
 import React from "react";
 
 const Weather = props => (
-    <div className="weather-block">
+    <div className="weather-info">
         <ul className="weather-list">
-            { props.city && props.country && <li className="weather-list-item">Location: { props.city }, { props.country }</li> }
-            { props.temperature && <li className="weather-list-item">Temperature: { props.temperature }</li> }
-            { props.humidity && <li className="weather-list-item">Humidity: { props.humidity }</li> }
-            { props.description && <li className="weather-list-item">Conditions: { props.description }</li> }
-            { props.error && <li className="weather-list-item text-error">{ props.error }</li> }
+            {
+                props.city && props.country && <li className="weather-list-item">Location: 
+                    <span className="weather-list-item__value"> { props.city }, { props.country }</span>
+                </li>
+            }
+            {
+                props.temperature && <li className="weather-list-item">Temperature: 
+                    <span className="weather-list-item__value"> { props.temperature }°</span>
+                </li>
+            }
+            {
+                props.humidity && <li className="weather-list-item">Humidity: 
+                    <span className="weather-list-item__value"> { props.humidity }%</span>
+                </li>
+            }
+            {
+                props.description && <li className="weather-list-item">Conditions: 
+                    <span className="weather-list-item__value"> { props.description }</span>
+                </li>
+            }
         </ul>
+        {
+            props.error && <p className="text-error"> { props.error }</p>
+        }
     </div>
 );
 
