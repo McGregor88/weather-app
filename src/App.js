d034aa3e15167df58de2e1eb37bc3bdd3e4b5620
Ruslan Igorevich <ruslan.igorevich@list.ru>
2018-04-22 17:01:49 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Titles />
+                <Form />
+                <Weather />
+            </div>
+        );
+    }
+};
 
 export default App;
